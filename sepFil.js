b14e80cd03c1fa58dd7e61be9ae39f6d217e2720
CarKraken <112105723+CarKraken@users.noreply.github.com>
2024-07-07 23:23:55 +0000

    make dictionary from js diaplsy in html

--- sepFil.js
+++ sepFil.js
@@ -1,4 +1,8 @@
 var uni = 0;
 
+//if doesn't work, maaaybe remove quotes around keys
 //put all the variables in an array so I can iterate through them!! :0
-const allEquipmentTypesArray = {BadmintonNet:0, BadmintonPoles:0};
+const allEquipmentTypesArray = {
+    "BadmintonNet":0, 
+    "BadmintonPoles":0,
+};
