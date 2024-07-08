b3642e9aa85999e52818db2fb7b5355215ff170b
CarKraken <112105723+CarKraken@users.noreply.github.com>
2024-07-08 01:00:23 +0000

    attempt to fix js resetting on new pg

--- sepFil.js
+++ sepFil.js
@@ -1,6 +1,5 @@
 var uni = 0;
 
-//if doesn't work, maaaybe remove quotes around keys
 //put all the variables in an array so I can iterate through them!! :0
 const allEquipmentTypesArray = {
     "BadmintonNet":0, 
