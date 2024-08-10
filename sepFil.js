b017466958e83d6d4d19cbe051bdb153c18672b9
CarKraken <112105723+CarKraken@users.noreply.github.com>
2024-08-10 01:40:55 +0000

    try test localStor fxn in js instead of html

--- sepFil.js
+++ sepFil.js
@@ -10,5 +10,16 @@ const allEquipmentTypesArray = {
 //try incremement allEquipArray while on main_sc and setItem to its array counterpart in same fxn
 localStorage.setItem("badPol", "0");
 
+function testLocStor(){
+    //ALT:
+    //localStorage.setItem("badPol") = 1 + parseInt(document.getElementById("bmn_pl"));
+    //localStorage.setItem("badPol") = 1 + parseInt(localStorage.getItem("badPol"));
+    //document.getElementById("bmn_pl").innerHTML = localStorage.getItem("badPol");
+
+    //ALT: following 2 lines
+    //works 1x, but it seems like badPool isn't incrementing past 0
+    document.getElementById("bmn_pl").innerHTML = parseInt(localStorage.getItem("badPol")) + 1;
+    localStorage.setItem("badPol") = getElementById("bmn_pl");
+}
 
 //clearn local storage after user sends finished log
