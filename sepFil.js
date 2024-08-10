ac2a70e90d369a38c47589313bc32baec21ebe1e
CarKraken <112105723+CarKraken@users.noreply.github.com>
2024-08-10 02:04:19 +0000

    trying localStor vars ++

--- sepFil.js
+++ sepFil.js
@@ -10,6 +10,8 @@ const allEquipmentTypesArray = {
 //try incremement allEquipArray while on main_sc and setItem to its array counterpart in same fxn
 localStorage.setItem("badPol", "0");
 
+//works whether in js or html file
+//FIX: doesn't increment past 1
 function testLocStor(){
     //ALT:
     //localStorage.setItem("badPol") = 1 + parseInt(document.getElementById("bmn_pl"));
@@ -18,8 +20,9 @@ function testLocStor(){
 
     //ALT: following 2 lines
     //works 1x, but it seems like badPool isn't incrementing past 0
-    document.getElementById("bmn_pl").innerHTML = parseInt(localStorage.getItem("badPol")) + 1;
-    localStorage.setItem("badPol") = getElementById("bmn_pl");
+    localStorage.getItem("badPol")++;
+    document.getElementById("bmn_pl").innerHTML = parseInt(localStorage.getItem("badPol"));
+    //localStorage.setItem("badPol") = getElementById("bmn_pl");
 }
 
 //clearn local storage after user sends finished log
